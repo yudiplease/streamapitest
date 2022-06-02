e856ae661654ba885688ded65c46941a305dd5b2
yudiplease <89980515+yudiplease@users.noreply.github.com>
2022-06-02 11:39:03 +0300

    Дз по StreamAPI

---
+++ StreamAPI/Student.java
@@ -0,0 +1,59 @@
+package StreamAPI;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int score;
+
+    public Student(String firstName, String lastName, int score) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        if(score > 100 || score < 0) {
+            System.out.println("Wrong score");
+            return;
+        }
+        this.score = score;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        if(score > 100 || score < 0) {
+            System.out.println("Wrong score");
+            return;
+        }
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student {" +
+                " firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", score=" + score +
+                " }";
+    }
+
+}
